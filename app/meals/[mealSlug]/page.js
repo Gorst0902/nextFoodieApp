4dcb6e5a58bcf96d4ceaba7e96b18dbe81374d5d
Gorst0902 <dinhvanthanhan0902@gmail.com>
2024-08-01 11:48:18 +0700

    update function get meals from db using sqlite3

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,34 @@
-import React from 'react'
 
-export default function MealDetailsPage() {
+import Image from 'next/image';
+import classes from './page.module.css';
+
+import { getMeal } from '@/lib/meal.js';
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
   return (
-    <div>Meal Details</div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions
+        }}></p>
+      </main>
+    </>
   )
 }
